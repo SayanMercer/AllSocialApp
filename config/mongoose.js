3bf5aaae390e135a73b6ee9ab5e86708202ed9ae
SayanMercer <dassayan14.1998@gmail.com>
2023-01-06 01:35:00 +0530

    Added database and loaded schema

---
+++ config/mongoose.js
@@ -0,0 +1,12 @@
+const mongoose = require ('mongoose');
+
+mongoose.connect('mongodb://127.0.0.1:27017/codeial_development');
+
+const db = mongoose.connection;
+db.on('error',console.error.bind(console,"Error connecting to MongoDB"));
+
+db.once('open',function(){
+    console.log('Connected to Database :: MongoDB');
+});
+
+module.exports =db;
--- index.js
+++ index.js
@@ -2,11 +2,13 @@ const express = require('express');
 const app = express();
 const port = 8000;
 const expressLayouts = require('express-ejs-layouts');
+const db = require('./config/mongoose');
 
 app.use(express.static('./assets'));
 
 app.use(expressLayouts);
 //extract style and scripts from pages into the layout
+
 app.set('layout extractStyles',true);
 app.set('layout extractScripts',true);
 
