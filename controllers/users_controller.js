483c227d4ab9932c07be94c3c87607c5ea6daac8
SayanMercer <dassayan14.1998@gmail.com>
2023-01-07 12:17:42 +0530

    Added sign in and sign up form

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -3,3 +3,23 @@ module.exports.profile = function(req,res){
         title : 'User Profile'
     })
 }
+
+// render the Sign up page
+module.exports.signUp = function(req,res){
+    return res.render('user_sign_up',{
+        title: "Codeial | Sign Up"
+    })
+
+}
+// Render the sign in page
+module.exports.signIn = function(req,res){
+    return res.render('user_sign_in',{
+        title: "Codeial | Sign In"
+    })
+}
+
+//get the sig up data
+
+module.exports.create = function(req,res){
+    //To do Later
+}
