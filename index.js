88920e3ad2b952d4029d16938074b23b4b98f9b2
SayanMercer <dassayan14.1998@gmail.com>
2023-01-08 12:57:39 +0530

    Create aspects of Signup page and added functionality

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require('express');
+const cookieParser = require('cookie-parser');
 const app = express();
 const port = 8000;
 const expressLayouts = require('express-ejs-layouts');
 const db = require('./config/mongoose');
 
+
+app.use(express.urlencoded());
+app.use(cookieParser());
 app.use(express.static('./assets'));
 
 app.use(expressLayouts);
@@ -15,6 +19,8 @@ app.set('layout extractScripts',true);
 //use express router
 
 app.use('/', require('./routes'));
+
+
 
 
 //set up the view engine
