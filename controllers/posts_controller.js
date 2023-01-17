8d51f8240b99ff0d7d0af004cdcb678a833abcd6
SayanMercer <dassayan14.1998@gmail.com>
2023-01-17 22:42:41 +0530

    post part added and shown to the display and the post creator is also added

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -2,7 +2,7 @@ const Post = require('../models/post')
 
 module.exports.create = function(req, res){
     Post.create({
-        content: req.body.cotent,
+        content: req.body.content,
         user: req.user._id
     },function(err,post){
 
