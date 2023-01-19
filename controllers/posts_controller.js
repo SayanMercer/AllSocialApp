b93dcd8887515420a53ea81f89ac3f4d6c8dee5d
SayanMercer <dassayan14.1998@gmail.com>
2023-01-19 13:02:51 +0530

    Post and comments created succesfully

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -4,7 +4,7 @@ module.exports.create = function(req, res){
     Post.create({
         content: req.body.content,
         user: req.user._id
-    },function(err,post){
+    }, function(err,post){
 
     if(err){console.log('error in creating a post'); return;}
        
--- models/post.js
+++ models/post.js
@@ -4,14 +4,21 @@ const mongoose = require('mongoose');
 const postSchema = new mongoose.Schema({
     content: {
         type: String,
-        require:true
+        required:true
 
     },
     user: {
         type: mongoose.Schema.Types.ObjectId,
         ref : 'User'
 
-        }
+        },
+        //include the array of ids of all comments in this post schema itself
+        comments:[
+            {
+                type: mongoose.Schema.Types.ObjectId,
+                ref : 'Comment'
+            }
+        ]
 },{
    timestamps: true
 
