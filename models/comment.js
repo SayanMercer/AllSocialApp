e0b074abf1aa8e0ff1711bbd77d85c1a25b72b90
SayanMercer <dassayan14.1998@gmail.com>
2023-02-01 22:17:59 +0530

    Delete comment Code is added

--- models/comment.js
+++ models/comment.js
@@ -21,5 +21,5 @@ const commentSchema = new mongoose.Schema({
 });
 
 
-const  Comment =mongoose.model('Comment',commentSchema);
+const  Comment = mongoose.model('Comment', commentSchema);
 module.exports = Comment;
