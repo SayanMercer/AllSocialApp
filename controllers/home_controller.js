87eb82abcb2c474271aa13404a2aefa91d31b857
SayanMercer <dassayan14.1998@gmail.com>
2023-02-03 20:07:57 +0530

    Friends section and Name and email_id update section is added

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,5 +1,7 @@
 const Post = require('../models/post');
 
+const User = require('../models/user');
+
 module.exports.home = function(req,res){
     // console.log(req.cookies);
     // res.cookie('user_id', 25);
@@ -20,9 +22,15 @@ module.exports.home = function(req,res){
         }
     })
     .exec(function(err, posts){
+
+       User.find({}, function(err, users){
         return res.render('home',{
             title:"Codeial | Home",
-            posts: posts
+            posts: posts,
+            all_users: users
+       });
+
+        
         });
     })
 
--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,9 +1,26 @@
 const User = require('../models/user');
 
 module.exports.profile = function(req,res){
-    return res.render('user_profile', {
-        title : 'User Profile'
-    })
+    console.log(req.params.id)
+    User.findById(req.params.id, function(err,user){
+        console.log(user)
+      return res.render('user_profile', {
+        title : 'User Profile',
+        profile_user: user
+      });
+    });
+}
+
+
+module.exports.update = function(req,res){
+    if(req.user.id == req.params.id){
+        User.findByIdAndUpdate(req.params.id, req.body, function(err,user){
+            return res.redirect('back');
+        });
+
+    }else{
+        return res.status(401).send('Unauthorized');
+    }
 }
 
 // render the Sign up page
@@ -59,7 +76,9 @@ module.exports.createSession = function(req,res){
 
 
 module.exports.destroySession = function(req, res){
-    req.logout();
+    req.logout(function(err){
+        return
+    });
 
     return res.redirect('/');
 }
