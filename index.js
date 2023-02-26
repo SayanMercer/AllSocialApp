807d9bf04f1fa2dda879b7bc59f75f8bf51b785e
SayanMercer <dassayan14.1998@gmail.com>
2023-02-27 01:22:38 +0530

    Multer added and file uploading process completed

--- index.js
+++ index.js
@@ -27,6 +27,8 @@ app.use(sassMiddleware({
 app.use(express.urlencoded());  
 app.use(cookieParser());
 app.use(express.static('./assets'));
+//make the uploads path available to the browser
+app.use('/uploads',express.static(__dirname + '/uploads'));
 
 app.use(expressLayouts);
 //extract style and scripts from pages into the layout
