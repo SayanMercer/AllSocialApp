85ddd3e2c8355ac778eb2b69ed7f15357a79c227
SayanMercer <dassayan14.1998@gmail.com>
2023-03-11 20:38:03 +0530

    Mail Sending feature Added

---
+++ config/nodemailer.js
@@ -0,0 +1,44 @@
+const nodemailer = require("nodemailer");
+const ejs = require('ejs');
+const path = require('path')
+console.log("nodemailer");
+let transporter = nodemailer.createTransport({
+    service: 'gmail',
+    host: 'smtp.gmail.com',
+    port: 587,
+    secure: false,
+    auth: {
+        user: 'mercerdas14@gmail.com',
+        pass:'bisyogrjuapeqifj'
+
+    }
+});
+// var transporter = nodemailer.createTransport({
+//     host: "sandbox.smtp.mailtrap.io",
+//     port: 2525,
+//     auth: {
+//       user: "4dc1811bb25e9b",
+//       pass: "16b913afb08c10"
+//     }
+//   });
+
+let renderTemplate = (data, relativePath) => {
+    let mailHTML;
+    ejs.renderFile(
+        path.join(__dirname, '../views/mailers', relativePath),
+        data,
+        function(err, template){
+        if(err){console.log('error in rendering template',err); return}
+
+        mailHTML=template;
+        }
+
+    )
+
+    return mailHTML;
+}
+
+module.exports = {
+    transporter: transporter,
+    renderTemplate: renderTemplate
+}
--- controllers/comments_controller.js
+++ controllers/comments_controller.js
@@ -1,20 +1,49 @@
 const Comment = require('../models/comment');
 const Post = require('../models/post');
+const commentsMailer = require('../mailers/comments_mailer');
+//const queue = require('../config/kue');
+//const commentEmailWorker = require('../workers/comment_email_worker');
+
 
 module.exports.create = async function(req,res){
- // console.log(req.body);
+  console.log(req.body);
 
   try{
       let post = await Post.findById(req.body.post);
+      console.log(post);
       if(post){
        let comment = await Comment.create({
             content: req.body.content,
             post: req.body.post,
             user: req.user._id
         }); 
-
+         //console.log("Commentcontroller");
           post.comments.push(comment);
           post.save();
+          //console.log(comment);
+
+          let newComment = await comment.populate("user"); // doubt
+          
+
+          commentsMailer.newComment(comment);
+          let job = queueMicrotask.create('emails',comment).save(function(err){
+            if(err){
+              console.log('error in creating a queue');
+            }
+
+            console.log(job.id)
+
+          });
+
+          if(req.xhr){
+            //commentsMailer.newComment(comment);
+            return res.status(200).json({
+              data: {
+                comment:comment
+              },
+              message: "Post Created!"
+            });
+          }
           req.flash('success','Comment Published!');
 
           res.redirect('/');
@@ -22,7 +51,9 @@ module.exports.create = async function(req,res){
       }
   }catch(err){
     req.flash('Error',err);
+    res.redirect('/');
     return;
+    
   }
 }
 
@@ -53,3 +84,4 @@ module.exports.destroy = async function(req, res){
   
   
 }
+
