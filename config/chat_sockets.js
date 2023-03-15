e2dd6042a67679b222a4b9601ecfdaf0307e2211
SayanMercer <dassayan14.1998@gmail.com>
2023-03-15 21:27:51 +0530

    Chatting Engine Full feature added

--- config/chat_sockets.js
+++ config/chat_sockets.js
@@ -1,6 +1,9 @@
 
 module.exports.chatSockets = function(socketServer){
-  let io = require('socket.io')(socketServer);
+ 
+    let io = require('socket.io')(socketServer, {cors: {
+        origin: "*", 
+        methods: ["GET", "POST"]}});
 
   io.sockets.on('connection', function(socket){
       console.log('new connection received', socket.id);
